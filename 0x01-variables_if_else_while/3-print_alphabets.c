4943a2b56b872654e256e0ca322eeea056341bbf
Olabisi <bisiadesujo@yahoo.com>
2022-09-12 06:52:21 -0700

    add script to print lower and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always;
+ */
+int main(void)
+{
+	char chl,chu;
+
+	chl = 'a';
+	chu = 'A';
+
+	while (chl <= 'z')
+	{
+		putchar(chl);
+		if (chl == 'z')
+		{
+			while (chu <= 'Z')
+			{
+				putchar(chu);
+				chu++;
+			}
+		}
+		chl++;
+	}
+	putchar('\n');
+	return (0);
+}
