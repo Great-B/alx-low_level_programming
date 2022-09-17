24a8c9304ed05bb6e1310d196a7aa18f90649762
Olabisi <bisiadesujo@yahoo.com>
2022-09-16 21:47:56 -0700

    add script for multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+/**
+ * mul - this function check if an input is a digit or not
+ * @a: the first input argument
+ * @b: the second input argument
+ * Description: to check if a character is upper case or not
+ *
+ * Return: an integer value as outputs
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
