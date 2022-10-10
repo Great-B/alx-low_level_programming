48ff4891115dbbebe2eaeded44c89ebe2d003861
Olabisi <bisiadesujo@yahoo.com>
2022-10-10 08:46:10 -0700

    add a function-like macro to compute sum of numbers

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,15 @@
+#include "4-sum.h"
+#include <stdio.h>
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+#ifndef ABS
+#define SUM(x, y) ((x) + (y))
+#endif
+
+#endif
