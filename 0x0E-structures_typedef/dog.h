e2325e78cc6304f766eaa9a9b4243b8d1522de53
Olabisi <bisiadesujo@yahoo.com>
2022-10-10 16:51:27 -0700

    define a structure name dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,27 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog 
+/**
+ * dog_t - Typedef of dog structure
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - a simple dog structure
+ * @owner: owner of dog
+ * @name: name of dog
+ * @age: age of dog
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
