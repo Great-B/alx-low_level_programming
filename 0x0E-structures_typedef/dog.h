9b6b6b760a125126644e122c8220691365a4a5a8
Olabisi <bisiadesujo@yahoo.com>
2022-10-10 15:30:34 -0700

    define a structure name dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/* list of prototypes */
+int _putchar(char c);
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
